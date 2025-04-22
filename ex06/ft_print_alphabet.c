dcac59c02de2daec87556a013270eba26950614d
Lea Nakache <lenakach@paul-f3Ar8s3.clusters.42paris.fr>
2025-04-22 18:57:16 +0200

    ok

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lenakach <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 17:40:32 by lenakach          #+#    #+#             */
+/*   Updated: 2025/04/22 17:46:27 by lenakach         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+/*void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}*/
+
+void	ft_print_alphabet(void)
+{
+	char c;
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		ft_putchar(c);
+		c++;
+	}
+}
+
+
+/*int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}*/
